57ba055dad6edecb41d39e5ce86caa8d73f44227
Kevin Silveira <k.ironhide9@yahoo.com>
2023-05-14 20:26:35 -0400

    created sign in and updated login html/css files

--- app/login.js
+++ app/login.js
@@ -4,10 +4,10 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
+    const emailAddress = loginForm.emailAddress.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (emailAddress === "Email Address" && password === "web_dev") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
